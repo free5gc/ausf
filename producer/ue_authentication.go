b2dcd719a1549de1429ac15a1e22a7f4b748457b
chliu-nems <87927057+chliu-nems@users.noreply.github.com>
2021-11-06 18:45:22 +0800

    fix send SUPI even if authentication is failure (#4)

--- producer/ue_authentication.go
+++ producer/ue_authentication.go
@@ -328,6 +328,7 @@ func Auth5gAkaComfirmRequestProcedure(updateConfirmationData models.Confirmation
 		responseBody.AuthResult = models.AuthResult_SUCCESS
 		success = true
 		logger.Auth5gAkaComfirmLog.Infoln("5G AKA confirmation succeeded")
+		responseBody.Supi = currentSupi
 		responseBody.Kseaf = ausfCurrentContext.Kseaf
 	} else {
 		ausfCurrentContext.AuthStatus = models.AuthResult_FAILURE
@@ -346,7 +347,6 @@ func Auth5gAkaComfirmRequestProcedure(updateConfirmationData models.Confirmation
 		return nil, &problemDetails
 	}
 
-	responseBody.Supi = currentSupi
 	return &responseBody, nil
 }
 
