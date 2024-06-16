c6ace2befdd30b48855f6b02bc73ec077d22599e
TYuan0816 <stevenchou5@gmail.com>
2024-06-17 03:17:07 +0800

    fix: add sorprotection and upuprotection routes

--- internal/sbi/api_sorprotection.go
+++ internal/sbi/api_sorprotection.go
@@ -6,6 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func (s *Server) getSorprotectionRoutes() []Route {
+	return []Route{
+		{
+			Method:  http.MethodGet,
+			Pattern: "/",
+			APIFunc: Index,
+		},
+		{
+			Method:  http.MethodPost,
+			Pattern: "/:supi/ue-sor",
+			APIFunc: s.SupiUeSorPost,
+		},
+	}
+}
+
 func (s *Server) SupiUeSorPost(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{})
 }
--- internal/sbi/api_upuprotection.go
+++ internal/sbi/api_upuprotection.go
@@ -6,6 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func (s *Server) getUpuprotectionRoutes() []Route {
+	return []Route{
+		{
+			Method:  http.MethodGet,
+			Pattern: "/",
+			APIFunc: Index,
+		},
+		{
+			Method:  http.MethodPost,
+			Pattern: "/:supi/ue-upu",
+			APIFunc: s.SupiUeUpuPost,
+		},
+	}
+}
+
 func (s *Server) SupiUeUpuPost(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{})
 }
